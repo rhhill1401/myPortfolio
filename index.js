394e3e6dad35e36ed8b8d112d6f3147a4634bdac
rhhill1401 <75366252+rhhill1401@users.noreply.github.com>
2021-02-17 23:48:05 -0600

    initialUpload

---
+++ index.js
@@ -0,0 +1,1 @@
+$('.new').addClass('bigAssTitle  alignment');
---
+++ script.js
@@ -0,0 +1,19 @@
+'use script';
+$(window).on('load', function () {
+  $('.fadeAway').hide();
+  $('.fadeAway').fadeIn(4000);
+});
+
+$(window).on('load', function () {
+  $('.fab').hide();
+  $('.fab').fadeIn(4000);
+});
+
+$('.portrait').addClass('.main-header');
+$('.display-4').css('color', 'white');
+$('.portaitCol').addClass('.portrait');
+$('.dropHome').css('background-color', '#fbe8d3');
+$('.primary1').css('background-color', '#a3d5ac');
+$('.primary2').css('background-color', '#928a97');
+$('.primary3').css('background-color', '#283c63');
+$('.salmon').css('background-color', '#f85f73');
